815a1be9a15bf6ac7446f17c60cd794c22e1f94e
safakun <safarovdeem@gmail.com>
2024-03-09 17:41:14 +0500

    added faq

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// FAQ Accordeon 
+document.addEventListener("DOMContentLoaded", () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+        
+        // toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // toggle visibility of body
+        groupBody.classList.toggle('open');
+        console.log(groupHeader);
+    });
+});
