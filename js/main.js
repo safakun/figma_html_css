ceb92dc74c2096769b990fbf9e875a78ec4b5017
safakun <safarovdeem@gmail.com>
2024-03-10 10:03:42 +0500

    added final styles and js

--- js/main.js
+++ js/main.js
@@ -32,3 +32,11 @@ document.addEventListener("DOMContentLoaded", () => {
        });
     });
 });
+
+// Mobile menu 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener("click", () => mobileMenu.classList.toggle('active'));
+})
